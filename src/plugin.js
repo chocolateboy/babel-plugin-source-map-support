4098b378195144199d0c89f59f8168cfa979c49c
David Carley <dacarley@gmail.com>
2016-06-17 12:59:28 -0700

    Paring it down to the bare minimum for our needs.

--- src/plugin.js
+++ src/plugin.js
@@ -1,36 +1,18 @@
-/**
- * add `import { install as _sourceMapSupport } from 'source-map-support';`
- * then call `_sourceMapSupport();`
- *
- * it equivalents to call `require('source-map-support/register');`,
- * but babel 6 `addImport` doesn't support it.
- *
- * @param t
- * @returns {{visitor: {Program: (function(*, *))}}}
- */
+module.exports = function(babel) {
+  var t = babel.types;
 
-export default ({types: t}) => {
+  return {
+    visitor: {
+      Program: {
+        exit: function(path, state) {
+          var importDeclaration = t.importDeclaration(
+            [],
+            t.stringLiteral("source-map-support/register")
+          );
 
-    return {
-        visitor: {
-            Program (path, {file}) {
-                let id;
-
-                id = file.addImport(
-                    'source-map-support',
-                    'install',
-                    '_sourceMapSupport'
-                );
-
-                path.traverse({
-                    ImportDeclaration(path) {
-
-                        if (path.node.source.value === 'source-map-support') {
-                            path.insertAfter(t.ExpressionStatement(t.CallExpression(t.identifier('_sourceMapSupport'), [])));
-                        }
-                    }
-                });
-            }
+          path.unshiftContainer('body', importDeclaration);
         }
-    };
+      }
+    }
+  };
 };
